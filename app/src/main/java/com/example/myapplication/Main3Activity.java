261a24d81289fccb3a2ae5d2ce32429865cdc43b
Afif Saifuddin <afifsyaifuddin99@gmail.com>
2020-05-11 23:43:44 +0800

    assestment 1

---
+++ app/src/main/java/com/example/myapplication/Main3Activity.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main3Activity.this, Main4Activity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
